5d9b67f63b1548bed6bee634e338e4fc734e9d5b
Miku Kawata <kawata_miku@lightcafe.co.jp>
2024-02-16 18:24:42 +0900

    feat: escapeキー押下で関数を呼び出すhookを作成

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,24 +1,13 @@
 import React, { useState } from 'react';
-
+import { useEscapeKey } from '../../hooks/use-escape-key.js';
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
   const [toasts, setToasts] = useState([]);
 
-  React.useEffect(() => {
-    const handleKeyDown = (e) => {
-      console.log(e);
-      if (e.code === 'Escape') {
-        setToasts([]);
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+  useEscapeKey(() => {
+    setToasts([]);
+  });
 
   const createToast = (message, variant) => {
     const newToasts = [...toasts, { id: new Date().getTime(), message, variant }];
---
+++ src/hooks/use-escape-key.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const useEscapeKey = (callback) => {
+  React.useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === 'Escape') {
+        callback();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+};
