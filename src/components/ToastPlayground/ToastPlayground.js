dbcf1a7f66221eff02c5492d62d71ba9611427d8
Miku Kawata <kawata_miku@lightcafe.co.jp>
2024-02-16 12:21:22 +0900

    feat: 複数のToastを表示できるように

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 
 import Button from '../Button';
-import Toast from '../Toast/Toast';
 
 import styles from './ToastPlayground.module.css';
+import ToastShelf from '../ToastShelf/ToastShelf';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [messageValue, setaMessageValue] = useState('');
   const [variantValue, setVariantValue] = useState(VARIANT_OPTIONS[0]);
-  const [displayToast, setDisplayToast] = useState(false);
+  const [toasts, setToasts] = useState([]);
+
+  const handlecreateToast = (e) => {
+    e.preventDefault();
+
+    const newToasts = [...toasts, { id: new Date().getTime(), message: messageValue, variant: variantValue }];
+    setToasts(newToasts);
+    setVariantValue(VARIANT_OPTIONS[0]);
+    setaMessageValue('');
+  };
+
+  const handleDismiss = (id) => {
+    const nextToasts = toasts.filter((toast) => toast.id !== id);
+    setToasts(nextToasts);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -18,13 +32,8 @@ function ToastPlayground() {
         <img alt='Cute toast mascot' src='/toast.png' />
         <h1>Toast Playground</h1>
       </header>
-      {displayToast && (
-        <Toast variant={variantValue} handleDismiss={() => setDisplayToast(false)}>
-          {messageValue}
-        </Toast>
-      )}
-
-      <div className={styles.controlsWrapper}>
+      <ToastShelf toasts={toasts} handleDismiss={handleDismiss} />
+      <form className={styles.controlsWrapper} onSubmit={handlecreateToast}>
         <div className={styles.row}>
           <label htmlFor='message' className={styles.label} style={{ alignSelf: 'baseline' }}>
             Message
@@ -56,10 +65,10 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button onClick={() => setDisplayToast(true)}>Pop Toast!</Button>
+            <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
